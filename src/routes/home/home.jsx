2ea16e3faebc758843015a8c9b496d09880b1260
andresfc01 <andresfernandezceacero@gmail.com>
2023-06-09 01:55:41 +0200

    diseño Home

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -2,7 +2,15 @@ import { Link, useNavigate } from "react-router-dom";
 import { getPlantillasMasUsadas } from "../../services/plantillas";
 import Plantilla from "../../components/plantilla/plantilla";
 import { useEffect, useState } from "react";
-import logoWk from "../../assets/images/logoHorizontal-bw.png";
+import logo from "../../assets/images/logoHorizontal.png";
+import styles from "./styles.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBarsProgress,
+  faCalendar,
+  faSignal,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   const navigate = useNavigate();
@@ -15,20 +23,60 @@ export default function App() {
     fetchPlantillas();
   }, []);
   return (
-    <>
-      <img src={logoWk} alt="" />
-      <h3>Disfruta y condigue tus objetivos</h3>
-      <button>
-        <Link to="/nuevaPlantilla">Nueva Plantilla</Link>
-      </button>
-      <h2>Plantillas de entrenamiento mas usadas</h2>
-      {plantillas &&
-        plantillas.map((plantilla) => (
-          <Plantilla key={plantilla._id} plantilla={plantilla} />
-        ))}
-      {plantillas && plantillas.length > 5 && (
-        <button onClick={() => navigate("/explorar")}>Explorar más</button>
-      )}
-    </>
+    <div className={styles.home}>
+      <section className={styles.cabeceraHome}>
+        <img src={logo} alt="" />
+        <h3>¡Mide y consigue tus objetivos físicos!</h3>
+      </section>
+      <section className={styles.beneficios}>
+        <h2>Beneficios</h2>
+        <div className={styles.beneficio}>
+          <FontAwesomeIcon icon={faCalendar} />
+          <div>
+            <h3>Planifica tus entrenamientos</h3>
+            <p>Haz tu rutina mas sencilla planificando todo tu progreso</p>
+          </div>
+        </div>
+        <div className={styles.beneficio}>
+          <FontAwesomeIcon icon={faBarsProgress} />
+          <div>
+            <h3>Progresa paso a paso</h3>
+            <p>Observa tu progreso y avanza paso a paso</p>
+          </div>
+        </div>
+        <div className={styles.beneficio}>
+          <FontAwesomeIcon icon={faUser} />
+          <div>
+            <h3>Comparte tus plantillas</h3>
+            <p>Comparte y prueba entrenamientos con tus amigos</p>
+          </div>
+        </div>
+      </section>
+      <section className={styles.llamadaAccion}>
+        <p>Comienza tu camino hacia un estilo de vida saludable</p>
+        <button className="btnPrincipal">
+          <Link to="/register">Regístrate ahora</Link>
+        </button>
+      </section>
+      <section className={styles.plantillas}>
+        <h2>Plantillas de entrenamientos mas utilizadas</h2>
+        <div>
+          {plantillas &&
+            plantillas.map((plantilla, cont) => {
+              if (cont < 6) {
+                return <Plantilla key={plantilla._id} plantilla={plantilla} />;
+              }
+            })}
+        </div>
+        {plantillas && plantillas.length > 5 && (
+          <button
+            className="btnPrincipal"
+            onClick={() => navigate("/explorar")}
+          >
+            Explorar más
+          </button>
+        )}
+      </section>
+    </div>
   );
 }
